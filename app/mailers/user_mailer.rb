726ca6e0376bdfed77384b415886c08134a15fa9
libao <libaohoo@gmail.com>
2015-01-01 22:25:47 +0800

    add bcc email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 #encoding: utf-8
 class UserMailer < ActionMailer::Base
-  default from: "no-reply@wasabiyangon.com"
+  default from: "no-reply@wasabiyangon.com",
+  				bcc: ["hervester9@gmail.com", "lb.funward@gmail.com"]
 
   def order_email(purchase)
   	@purchase = purchase
