66af6e07c59476f9338f20ae51f3cc361d5a02f5
EC2 Default User <ec2-user@ip-172-31-34-117.us-west-2.compute.internal>
2015-07-30 04:07:28 +0000

    server configuration

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -12,7 +12,7 @@ set -e
  
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/alpha/workspace/myPro/wasabi
+APP_ROOT=/home/ec2-user/var/www/wasabi
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.conf -E production"
 AS_USER=<user>
